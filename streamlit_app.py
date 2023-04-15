43d4a441bf37f26141a0ef7a706c002d883defc6
krolowapolskilab <130828211+krolowapolskilab@users.noreply.github.com>
2023-04-15 10:41:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,23 @@ streamlit.write("🥑🍞 Avocado Toast 🥑🍞")
 # Special menu item
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+import streamlit as st
+import pandas as pd
 
-# Display the table on the page.
+# Create a list of fruits to choose from
+fruit_options = ['Apple', 'Banana', 'Blueberry', 'Cherry', 'Kiwi', 'Mango', 'Pineapple', 'Strawberry']
+
+# Add a Multi-select widget to the app
+selected_fruits = st.multiselect('Select your fruits:', fruit_options)
+
+# Read CSV file into dataframe
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Filter the dataframe to show only the selected fruits
+filtered_fruit_list = my_fruit_list[my_fruit_list['Fruit'].isin(selected_fruits)]
+
+# Display the filtered dataframe
+st.dataframe(filtered_fruit_list)
 
 # Read CSV file into dataframe
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
