2e8e15dd287b34dbd3b4c04f9c88bd9a7e1e7b73
krolowapolskilab <130828211+krolowapolskilab@users.noreply.github.com>
2023-04-15 10:21:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 def main():
-    st.title('My Parents New Healthy Diner')
+    st.title("My Mom's New Healthy Diner")
     
     st.header('Breakfast Menu')
     
-    st.write('Omega 3 & Blueberry Oatmeal')
+    st.write('🥣 Omega 3 & Blueberry Oatmeal')
     
-    st.write('Kale, Spinach & Rocket Smoothie')
+    st.write('🥗 Kale, Spinach & Rocket Smoothie')
     
-    st.write('Hard-Boiled Free-Range Egg')
+    st.write('🐔 Hard-Boiled Free-Range Egg')
 
 if __name__ == '__main__':
     main()
