76a5bc4805536c913d6364903ed8d71280fa9387
krolowapolskilab <130828211+krolowapolskilab@users.noreply.github.com>
2023-04-15 10:44:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,17 @@
-import streamlit
-import pandas
-
-streamlit.title("My Mom's New Healthy Diner")
-
-# Breakfast menu
-streamlit.header("Breakfast Menu")
-streamlit.write("🥣 Omega 3 & Blueberry Oatmeal")
-streamlit.write("🥗 Kale, Spinach & Rocket Smoothie")
-streamlit.write("🐔 Hard-Boiled Free-Range Egg")
-streamlit.write("🥑🍞 Avocado Toast 🥑🍞")
-
-# Special menu item
-streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
-
 import streamlit as st
 import pandas as pd
+
+st.title("My Mom's New Healthy Diner")
+
+# Breakfast menu
+st.header("Breakfast Menu")
+st.write("🥣 Omega 3 & Blueberry Oatmeal")
+st.write("🥗 Kale, Spinach & Rocket Smoothie")
+st.write("🐔 Hard-Boiled Free-Range Egg")
+st.write("🥑🍞 Avocado Toast 🥑🍞")
+
+# Special menu item
+st.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
 # Create a list of fruits to choose from
 fruit_options = ['Apple', 'Banana', 'Blueberry', 'Cherry', 'Kiwi', 'Mango', 'Pineapple', 'Strawberry']
@@ -30,11 +27,3 @@ filtered_fruit_list = my_fruit_list[my_fruit_list['Fruit'].isin(selected_fruits)
 
 # Display the filtered dataframe
 st.dataframe(filtered_fruit_list)
-
-# Read CSV file into dataframe
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-# Display dataframe
-streamlit.dataframe(my_fruit_list)
-
-
