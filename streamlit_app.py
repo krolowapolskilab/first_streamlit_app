55d3d63b3e6ab145d39401c3f3ffc595fd183943
krolowapolskilab <130828211+krolowapolskilab@users.noreply.github.com>
2023-04-15 10:17:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
-import streamlit 
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+def main():
+    st.title('My Parents New Healthy Diner')
+    
+    st.header('Breakfast Menu')
+    
+    st.write('Omega 3 & Blueberry Oatmeal')
+    
+    st.write('Kale, Spinach & Rocket Smoothie')
+    
+    st.write('Hard-Boiled Free-Range Egg')
+
+if __name__ == '__main__':
+    main()
