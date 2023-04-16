91bf2026011ea27de3faf906944452238b8e1853
krolowapolskilab <130828211+krolowapolskilab@users.noreply.github.com>
2023-04-16 19:01:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,8 @@ import snowflake.connector
 import streamlit as st
 import snowflake.connector
 
+python -m pip install --upgrade pip
+
 # Use Streamlit secrets to get Snowflake credentials
 SNOWFLAKE_CREDS = st.secrets["snowflake"]
 
