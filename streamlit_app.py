80f6315c3d267e3ce1c5c1ccd4b3477b07292a5a
krolowapolskilab <130828211+krolowapolskilab@users.noreply.github.com>
2023-04-16 19:49:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,3 +73,10 @@ try:
     cnx.close()
 except Exception as e:
     st.write("Connection failed: ", e)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
