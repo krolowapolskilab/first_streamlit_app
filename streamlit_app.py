0d0710587313eee449c6a654aa8425b6f5448487
krolowapolskilab <130828211+krolowapolskilab@users.noreply.github.com>
2023-04-16 20:37:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,3 +78,11 @@ try:
     cnx.close()
 except Exception as e:
     st.write("Connection failed: ", e)
+
+    Can you fix this code
+
+#Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit);
+with my_cnx.cursor() as my_cur;
+my_cur.execute("insert into fruit_load_list values("jackfruit", "papaya", "guava", "kiwi")
+return "Thanks for adding" + new_fruit
